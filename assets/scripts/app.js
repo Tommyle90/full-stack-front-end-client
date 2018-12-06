b9f8fc3708ac6cbda75a5f60564e5e4aa62b257d
tommyle90 <tommy.le1004@gmail.com>
2018-12-06 17:13:34 -0500

    Form and Log in display
    Added hidden classes to show and hide depending on certain button clicks
    Added so that user can sign in out and change password

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const authEvents = require('./auth/events.js')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +7,29 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-btn').on('click', authEvents.onSignOut)
+})
+
+$('#sign-up-btn').click(function () {
+  $('#sign-up-form').fadeIn(500)
+  $('#sign-in-form').fadeOut(500)
+  $('#change-password-form').fadeOut(500)
+  $('#message').show(500)
+})
+
+$('#sign-in-btn').click(function () {
+  $('#sign-in-form').fadeIn(500)
+  $('#sign-up-form').fadeOut(500)
+  $('#change-password-form').fadeOut(500)
+  $('#message').show(500)
+})
+
+$('#change-pass-btn').click(function () {
+  $('#change-password-form').fadeIn(500)
+  $('#sign-up-form').fadeOut(500)
+  $('#sign-in-form').fadeOut(500)
+  $('#message').show(500)
 })
