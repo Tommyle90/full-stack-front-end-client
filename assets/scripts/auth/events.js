065d10356f8a08d807948fb46fb19e3e47e9b4d7
tommyle90 <tommy.le1004@gmail.com>
2018-12-06 17:01:43 -0500

    Added Auth folder
    Added Authorization and styling to my frontend

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields.js')
+const api = require('./api.js')
+const ui = require('./ui.js')
+
+const onSignUp = (event) => {
+  event.preventDefault()
+  const userData = getFormFields(event.target)
+  $(event.target).trigger('reset')
+  api.signUp(userData)
+    .then(ui.signUpSuccess)
+    .catch(ui.failure)
+}
+//
+const onSignIn = (event) => {
+  event.preventDefault()
+  const userData = getFormFields(event.target)
+  $(event.target).trigger('reset')
+  api.signIn(userData)
+    .then(ui.signInSuccess)
+    .catch(ui.failure)
+}
+
+const onChangePassword = (event) => {
+  event.preventDefault()
+  const userData = getFormFields(event.target)
+  $(event.target).trigger('reset')
+  api.changePassword(userData)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.failure)
+}
+
+const onSignOut = () => {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.failure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
---
+++ curl-scripts/sign-up.sh
@@ -0,0 +1,14 @@
+
+curl "https://desolate-bayou-43312.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
